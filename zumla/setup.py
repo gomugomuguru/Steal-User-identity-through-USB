2d8fd0d1c80175b55531f09277993f8aeda7a8ec
Ganesh Lipane <33823264+gamblur@users.noreply.github.com>
2017-11-27 11:30:17 +0530

    appended py2exe argument directly

--- zumla/setup.py
+++ zumla/setup.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 from distutils.core import setup
-import py2exe
+import py2exe, sys, os
+
+sys.argv.append('py2exe')
 
 setup(
+    options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
     windows=["mal.pyw"],
     zipfile=None
      )
